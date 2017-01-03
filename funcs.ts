d01346e8c7f011d7f84c95cce38fb471f8371a99
Shlomibo <shlomi.newmail@gmail.com>
2017-01-03 23:13:30 +0200

    Experimental callback hook

--- funcs.ts
+++ funcs.ts
@@ -1,3 +1,38 @@
 export function identity<T>(x: T): T {
 	return x;
 }
+
+export interface CallbackHandler<T> extends PromiseLike<T> {
+	(this: void, err: any, result: T): void;
+}
+export interface MultiArgCallbackHandler<T> extends PromiseLike<T[]> {
+	(this: void, err: any, ...results: T[]): void;
+}
+
+export function fromCallback<T>(multiArg: false): CallbackHandler<T>;
+export function fromCallback<T>(multiArg: true): MultiArgCallbackHandler<T>;
+export function fromCallback<T>(multiArg: boolean)
+	: CallbackHandler<T> | MultiArgCallbackHandler<T> {
+
+	let handler: any;
+	const resolvePromise = new Promise((resolve, reject) => {
+		handler = (err: any, ...results: T[]) => {
+			if (err) {
+				reject(err);
+			}
+			else {
+				resolve(
+					multiArg
+						? results
+						: results[0]
+				);
+			}
+		};
+		handler.then =
+			(onFullfilled: any, onRejected: any) => resolvePromise.then(
+				onFullfilled,
+				onRejected
+			);
+	});
+	return handler;
+}
