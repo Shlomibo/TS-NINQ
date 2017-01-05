2d0e10bca2a91194da170eada10656b266c817fa
Shlomibo <shlomi.newmail@gmail.com>
2017-01-05 20:03:18 +0200

    ...

--- operators/traverse.ts
+++ operators/traverse.ts
@@ -1,13 +1,14 @@
 
-export interface FirstTraverseMapping<T> {
+export interface TraverseMappingBase<T> {
 	current: T;
 	index: number;
+	last: boolean;
+}
+export interface FirstTraverseMapping<T> extends TraverseMappingBase<T> {
 	previous: undefined;
 	first: true;
 }
-export interface LaterTraverseMapping<T> {
-	current: T;
-	index: number;
+export interface LaterTraverseMapping<T> extends TraverseMappingBase<T> {
 	previous: T;
 	first: false;
 }
@@ -19,17 +20,41 @@ export class TraversingIterable<T> implements Iterable<TraverseMapping<T>> {
 	*[Symbol.iterator](): Iterator<TraverseMapping<T>> {
 		let previous: T | undefined,
 			first = true,
-			index = 0;
-		for (let item of this.it) {
-			yield {
-				current: item,
-				index,
-				first,
-				previous,
-			} as TraverseMapping<T>;
+			index = 0,
+			it = this.it[Symbol.iterator](),
+			current = it.next(),
+			next = !current.done && it.next();
+		try {
+			for (; !current.done
+				; current = next as any, next = it.next()) {
 
-			first = false;
-			previous = item;
+				try {
+					const item = current.value;
+
+					yield {
+						current: item,
+						index,
+						first,
+						previous,
+						last: !next || next.done
+					} as TraverseMapping<T>;
+
+					first = false;
+					previous = item;
+				}
+				catch (e) {
+					if (it.throw) {
+						current = next as any;
+						next = it.throw(e);
+					}
+					else {
+						throw e;
+					}
+				}
+			}
+		}
+		finally {
+			it && it.return && it.return();
 		}
 	}
 }
--- tests/operators/traverse.ts
+++ tests/operators/traverse.ts
@@ -5,23 +5,29 @@ describe('Traverses an iterable', () => {
 
 
 	it('traverse the array', () => {
-		let hadFirst = false;
+		let hadFirst = false,
+			hadLast = false;
 		_.of(arr)
 			.traverse()
 			.forEach(trav => {
+				if (hadLast) {
+					fail('had last');
+				}
 				if (trav.first && hadFirst) {
-					fail();
+					fail('duplicate first');
 				}
 				if (trav.first && trav.previous) {
-					fail();
+					fail('previous with first');
 				}
 				hadFirst = hadFirst || trav.first;
+				hadLast = hadLast || trav.last;
 
 				if (!trav.first) {
 					expect(trav.previous).toBe(trav.current - 1);
 				}
 			});
 		expect(hadFirst).toBe(true);
+		expect(hadLast).toBe(true);
 	});
 
 });
